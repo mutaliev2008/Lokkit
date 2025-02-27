e2f482d79e10fc884aea6ff5d6cec9466e1d8e82
Магомед-Башир <mutalievm350@gmail.com>
2025-02-27 14:03:42 +0300

    feat(directive): add SubscribeActiveButtonDirective for button state

--- src/app/features/users/components/user-card.component.ts
+++ src/app/features/users/components/user-card.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, Input } from '@angular/core';
 import { UserService } from '../../../core/services/user.service';
 import { CommonModule } from '@angular/common';
 import { User } from '../../../core/models/user.model';
+import { SubscribeActiveButtonDirective } from '../../../shared/directives/subscribe-active-button.directive';
 
 @Component({
   selector: 'app-user-card',
-  imports: [CommonModule],
+  imports: [CommonModule, SubscribeActiveButtonDirective],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.css',
 })
@@ -16,5 +17,9 @@ export class UserCardComponent {
   userService = inject(UserService);
   ngOnInit(): void {
     this.userService.getAllUsers();
+  }
+
+  toggleButton(): void {
+    this.buttonState = !this.buttonState;
   }
 }
