e4b619f1b8a201141e3fe5a2d4e029602e1263e4
Магомед-Башир <mutalievm350@gmail.com>
2025-03-14 03:51:09 +0300

    feat(edit-post): добавление логики редактирования поста в компоненте EditPostFormComponent

--- src/app/features/post/component/edit-post-form/edit-post-form.component.ts
+++ src/app/features/post/component/edit-post-form/edit-post-form.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PostService } from '../../../../core/services/post.service';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-post-form',
   imports: [],
   templateUrl: './edit-post-form.component.html',
-  styleUrl: './edit-post-form.component.css'
+  styleUrl: './edit-post-form.component.css',
 })
 export class EditPostFormComponent {
+  postService = inject(PostService);
+  route = inject(ActivatedRoute);
+  postId!: number;
 
+  editPostForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    content: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    image: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    tags: new FormArray([]),
+  });
+
+  ngOnInit(): void {
+    this.postId = +this.route.snapshot.paramMap.get('id')!;
+  }
+
+  editPost() {
+    if (this.editPostForm.valid) {
+      this.postService.editPost(this.postId, {
+        title: this.editPostForm.value.title!,
+        post_content: this.editPostForm.value.content!,
+        post_image: this.editPostForm.value.image!,
+        hashtags: this.editPostForm.value.tags || [],
+      });
+    }
+  }
+
+  get tags(): FormArray {
+    return this.editPostForm.get('tags') as FormArray;
+  }
+  uploadingData() {
+    const post = this.postService.getPostById(this.postId);
+    if (post) {
+      this.tags.clear();
+
+      if (post.hashtags) {
+        post.hashtags.forEach((tag) => this.tags.push(new FormControl(tag)));
+      }
+      this.editPostForm.patchValue({
+        title: post.title,
+        content: post.post_content,
+        image: post.post_image,
+      });
+    }
+  }
 }
